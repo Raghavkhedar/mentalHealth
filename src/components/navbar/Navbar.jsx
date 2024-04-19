228e055b0748faf76ce9c62d74df72209deaef69
Raghavkhedar <raghavkhedar528@gmail.com>
2024-04-19 21:54:01 +0530

    basic structure set dowo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import styles from './navbar.module.css'
+import heart from '../../assets/heart-wings-angel-svgrepo-com.png'
+
 const Navbar = () => {
     return (
-            <div className={styles.navbar}>Navbar</div>
+        <div className={styles.navbar}>
+            <div className = {styles.container}>
+
+                <div className ={styles.logo}>
+
+                    <img  src={heart} alt="" style={{height:'10rem', width: '10rem'}}/>
+
+                </div>
+
+                <div >
+                    <ul className={styles.options}>
+                        <li>About Us</li>
+                        <li>Resources</li>
+                        <li>Support</li>
+                        <li>Counselling</li>
+                        <li>Wellness Tools</li>
+                        <li>Blog</li>
+                        <li>Events</li>
+                        <li>Contacts Us</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default Navbar
