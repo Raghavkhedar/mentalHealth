c181acaeb0637ddfcc75c8dc0c02872cc3d03c5f
Raghavkhedar <raghavkhedar528@gmail.com>
2024-04-20 23:31:28 +0530

    hero2

--- src/components/hero2/Hero2.jsx
+++ src/components/hero2/Hero2.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import styles from './hero2.module.css'
+
 const Hero2 = () => {
-    return (
-        <div className={styles.hero}>
-            <div className={styles.main}>
-                <h1>Introducing MindWell</h1>
-                <p>Taking good care of your mental health has never been <br/>more important. 
-                Here’s a quick video about how Calm Health<br/> can help.</p>
+    return (<div className={styles.hero}>
+        <div className={styles.container}>
+            <div className={styles.subset}>
+                <div className={styles.main}>
+                    <h1 className={styles.heading}>Introducing MindWell</h1>
+                    <p className={styles.para}>Taking good care of your mental health has never been <br/>more important.
+                        Here’s a quick video about how Calm Health<br/> can help.</p>
+                </div>
+
+                <div className={styles.video}><VideoPlayer/>
+                </div>
+
             </div>
-            <div className={styles.video}><VideoPlayer /></div>
         </div>
-    )
+    </div>)
 }
+
 function VideoPlayer() {
-    return (
-      <video width="320" height="240" controls poster="./assests/cover.png">
-        <source src="movie.mp4" type="video/mp4" />
-        <source src="movie.ogg" type="video/ogg" />
+    return (<video width="320" height="240" controls poster="./assests/cover.png">
+        <source src="movie.mp4" type="video/mp4"/>
+        <source src="movie.ogg" type="video/ogg"/>
         Your browser does not support the video tag.
-      </video>
-    );
-  }
-  
+    </video>);
+}
+
 
 export default Hero2
