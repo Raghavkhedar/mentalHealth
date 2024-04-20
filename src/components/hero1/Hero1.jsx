b7890b1c1edabaa4beaccd92b8ae60e04e34d066
avya <avyaahuja910@gmail.com>
2024-04-20 15:47:29 +0530

    for raghav

--- src/components/hero1/Hero1.jsx
+++ src/components/hero1/Hero1.jsx
@@ -1,7 +1,18 @@
 import styles from './hero1.module.css'
+
 const Hero1 = () => {
     return (
-        <div className={styles.hero}>Hero</div>
+       
+        <div className={styles.hero}>
+            {/* <div className={styles.opa}> */}
+            <div className={styles.main}><h1 className={styles.h1}>Because mind and body are <br/>connected</h1></div>
+            <div className={styles.over}><p className={styles.p}>Calm Health offers evidence-based mental health programs
+             and tools<br/> focused on anxiety and depression that are designed to support <br/>payers,
+              plan sponsors, and providers.</p></div>
+            <div><button className={styles.button} href="#">Request Information</button></div>
+            {/* </div> */}
+        </div>
+       
     )
 }
 export default Hero1
