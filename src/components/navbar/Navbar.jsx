1940dccac4faa57720bb80c3348b62abbe5f2094
Raghavkhedar <raghavkhedar528@gmail.com>
2024-04-20 19:32:11 +0530

    pata nhi kya

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import styles from './navbar.module.css'
 const Navbar = () => {
     return (
-            <div className={styles.navbar}>Navbar</div>
+            <div className={styles.navbar}>
+
+                <div className={styles.container1}>
+                    <img src="" alt=""/>
+                </div>
+
+                <div className={styles.container2}>
+                    <ul className={styles.marker}>
+                        <li className={styles.mid}>About Us</li>
+                        <li className={styles.mid}>Resources</li>
+                        <li className={styles.mid}>Wellness Tools</li>
+                        <li className={styles.mid}>Blog</li>
+                    </ul>
+                </div>
+
+                <div className={styles.container3}>
+                    <button className={styles.custom}>Log in</button>
+                </div>
+
+            </div>
     )
 }
 export default Navbar
