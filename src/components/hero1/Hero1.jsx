a35134e4e5355e669b17a38b9d0b0a1ce0bf0f04
Raghavkhedar <raghavkhedar528@gmail.com>
2024-04-20 20:45:51 +0530

    heor 2 kinda fixed

--- src/components/hero1/Hero1.jsx
+++ src/components/hero1/Hero1.jsx
@@ -2,17 +2,22 @@ import styles from './hero1.module.css'
 
 const Hero1 = () => {
     return (
-       
+
         <div className={styles.hero}>
-            {/* <div className={styles.opa}> */}
-            <div className={styles.main}><h1 className={styles.h1}>Because mind and body are <br/>connected</h1></div>
-            <div className={styles.over}><p className={styles.p}>Calm Health offers evidence-based mental health programs
-             and tools<br/> focused on anxiety and depression that are designed to support <br/>payers,
-              plan sponsors, and providers.</p></div>
-            <div><button className={styles.button} href="#">Request Information</button></div>
-            {/* </div> */}
+            <div className={styles.main}>
+                <h1 className={styles.heading1}>
+                    Because mind and body are connected
+                </h1>
+
+                <h1 className={styles.heading2}>
+                    Calm Health offers evidence-based mental health programs and tools focused on anxiety and depression
+                    that are designed to support payers, plan sponsors, and providers.
+                </h1>
+
+                <button className={styles.button} href="#">Request Information</button>
+            </div>
         </div>
-       
+
     )
 }
 export default Hero1
