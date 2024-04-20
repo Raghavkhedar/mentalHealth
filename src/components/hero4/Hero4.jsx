92f40c44a5900fbd0398ae9e8fd143b03bd3a58c
Tanya-Sood <tanyasood1108@gmail.com>
2024-04-20 21:34:29 +0530

    hero4

--- src/components/hero4/Hero4.jsx
+++ src/components/hero4/Hero4.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import styles from './hero4.module.css'
 const Hero4 = () => {
     return (
-        <div className = {styles.hero}>Hero4</div>
+        <div className = {styles.hero}>
+            <div className={styles.container}>
+                <h1 className={styles.text}>Want to learn more?</h1>
+                <button className={styles.button}>Request Information</button>
+            </div>
+        </div>
     )
 }
 export default Hero4
