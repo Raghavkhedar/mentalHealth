b730be37397f206d364d58246757b70bb6348926
avya <avyaahuja910@gmail.com>
2024-04-20 19:53:36 +0530

    abort

--- src/components/hero2/Hero2.jsx
+++ src/components/hero2/Hero2.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styles from './hero2.module.css'
 const Hero2 = () => {
     return (
-        <div className={styles.hero}>Hero2</div>
+        <div className={styles.hero}>
+            <div><h1>Introducing MindWell</h1></div>
+            <div><p>Taking good care of your mental health has never been more important. 
+                Here’s a quick video about how Calm Health can help.</p></div>
+        </div>
     )
 }
 export default Hero2
