dcb5b390755d243562182355968f5e0d893f9228
avya <avyaahuja910@gmail.com>
2024-04-20 20:46:28 +0530

    hero2

--- src/components/hero2/Hero2.jsx
+++ src/components/hero2/Hero2.jsx
@@ -3,10 +3,24 @@ import styles from './hero2.module.css'
 const Hero2 = () => {
     return (
         <div className={styles.hero}>
-            <div><h1>Introducing MindWell</h1></div>
-            <div><p>Taking good care of your mental health has never been <br/>more important. 
-                Here’s a quick video about how Calm Health<br/> can help.</p></div>
+            <div className={styles.main}>
+                <h1>Introducing MindWell</h1>
+                <p>Taking good care of your mental health has never been <br/>more important. 
+                Here’s a quick video about how Calm Health<br/> can help.</p>
+            </div>
+            <div className={styles.vid}><VideoPlayer /></div>
         </div>
     )
 }
+function VideoPlayer() {
+    return (
+      <video width="320" height="240" controls poster="./assests/cover.png">
+        <source src="movie.mp4" type="video/mp4" />
+        <source src="movie.ogg" type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+    );
+  }
+  
+
 export default Hero2
