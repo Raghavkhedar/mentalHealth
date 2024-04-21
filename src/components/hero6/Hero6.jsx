bf2a4a96e78b63b78904c786fd50133a7f968f58
Raghavkhedar <raghavkhedar528@gmail.com>
2024-04-21 06:04:43 +0530

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/components/hero6/Hero6.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './hero6.module.css'
-const Hero6 = () => {
-    return (
-        <div className = {styles.hero}>Hero4</div>
-    )
-}
-export default Hero6
