f9ae8f778f2def78d7d7291c9bfdab5a18c25d4f
Tanya-Sood <tanyasood1108@gmail.com>
2024-04-21 05:36:21 +0530

    push

--- src/components/hero4/Hero4.jsx
+++ src/components/hero4/Hero4.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './hero4.module.css'
 const Hero4 = () => {
+    const handleClick = () => {
+        window.open('https://forms.gle/5q1EiJUNZzWZFFfw5', 'Request Information');
+    }
     return (
         <div className = {styles.hero}>
             <div className={styles.container}>
                 <h1 className={styles.text}>Want to learn more?</h1>
-                <button className={styles.button}>Request Information</button>
+                <button className={styles.button} onClick={handleClick}>Request Information</button>
             </div>
         </div>
     )
